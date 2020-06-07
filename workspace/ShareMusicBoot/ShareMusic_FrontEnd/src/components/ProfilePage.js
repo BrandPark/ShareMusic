51406a9a61da8b4d366e260d1cdc79cf55ea8070
gudtnzzang <hs950823@gmail.com>
2020-06-07 19:52:00 +0900

    [합병]페이징처리 수정

---
+++ workspace/ShareMusicBoot/ShareMusic_FrontEnd/src/components/ProfilePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfilePage extends Component {
+    
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default ProfilePage;
